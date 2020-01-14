a582c4f911c19a6ab95102c84a2807df05dd18fa
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-14 03:26:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 # load_library('lib/emoticons.yml')
 
 def get_japanese_emoticon(file_path, eng_emoticon)
-  if !load_library(file_path)[get_emoticon][eng_emoticon]
+  if !load_library(file_path)[:get_emoticon][eng_emoticon]
     puts "Sorry, I don't have a Japanese emotion for #{eng_emoticon}! Try again?"
   else
     load_library(file_path)[get_emoticon][eng_emoticon]
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, eng_emoticon)
 end
 
 def get_english_meaning(file_path, jap_emoticon)
-  if !load_library(file_path)[get_meaning][jap_emoticon]
+  if !load_library(file_path)[:get_meaning][jap_emoticon]
     puts "Sorry, even I don't know what #{jap_emoticon} means! Try again?"
   else
     load_library(file_path)[get_meaning][jap_emoticon]
