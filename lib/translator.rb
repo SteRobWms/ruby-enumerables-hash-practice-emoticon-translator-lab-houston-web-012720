dafeeda3d530ae34ae726aaedbe20599c532d55d
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-14 03:16:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,8 @@ def load_library(file_path)
   loadme.each_key do |k|
     translatorhash[:get_meaning][loadme[k][1]] = k
   end
+  loadme.each_key do |j|
+    translatorhash[:get_emoticon][loadme[k][0]] = loadme[k][1]
   pp translatorhash
 
 end
