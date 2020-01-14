7921d0293cbcd9bac3b19f9648089de723253e2d
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-14 03:10:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,9 +5,9 @@ require 'yaml'
 
 def load_library(file_path)
   loadme = YAML.load_file(file_path)
-  translatorhash = {get_meaning: {}, get_emoticon: {}}
+  translatorhash = {get_meaning => {}, get_emoticon => {}}
   loadme.each_key do |k|
-    translatorhash[get_meaning][k][1]: k
+    translatorhash[get_meaning][k][1] = k
   puts translatorhash
   end
 
